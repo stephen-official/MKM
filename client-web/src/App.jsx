aa0d6abab8b18e2f49d6b6bfe27cbc4985d499fd
stephen-official <stephenofficial4a6@gmail.com>
2026-07-03 00:25:02 +0530

    fixxx

--- client-web/src/App.jsx
+++ client-web/src/App.jsx
@@ -836,7 +836,7 @@ import { jwtDecode } from "jwt-decode";
 
 import Login from "./pages/Login";
 import ClientStore from "./components/ClientStore";
-// import { ClientStore } from "./components/ClientStore";
+
 import { socket, connectSocket, disconnectSocket } from "./socket";
 
 const ProtectedRoute = ({ children }) => {
